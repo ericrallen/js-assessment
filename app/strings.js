1a4c8c1a2b59c20970270b486f68cc236e0b0a85
Eric Allen <eric.allen@skookum.com>
2019-01-25 11:47:38 -0500

    added wordwrap solution

--- app/strings.js
+++ app/strings.js
@@ -43,8 +43,36 @@ exports.stringsAnswers = {
   },
 
   wordWrap: function(str, cols) {
-    // TODO: make this work
-    return str;
+    // there is probably an easier way to achieve this one, but ¯\_(ツ)_/¯
+ 
+    // lets start by getting each word separated
+    const words = str.split(' ');
+
+    // this will store each line in our newly wrapped string
+    let lines = [];
+
+    // this will store each word in our current line
+    let currentLine = [];
+
+    // iterate through our words
+    words.forEach((item) => {
+      // if we're going to surpass the line length
+      if (currentLine.length && currentLine.join(' ').length + item.length >= cols) {
+        // we need to start a new line
+        lines.push(currentLine.join(' '));
+
+        currentLine = [item];
+      } else {
+        // otherwise, just add the current word to the current line
+        currentLine.push(item);
+      }
+    });
+
+    // make sure we add any leftover words in the last line to the final string
+    lines.push(currentLine);
+
+    // join our lines with linebreaks and return the wrapped sting
+    return lines.join('\n');
   },
 
   reverseString: function(str) {
