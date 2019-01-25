2f3ffea3046ceafcab0b40b341f37e8af385ea66
Eric Allen <eric.allen@skookum.com>
2019-01-25 11:47:38 -0500

    added wordwrap solution

--- app/count.js
+++ app/count.js
@@ -2,21 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
-    // TODO: Figure out why I'm dumb and don't understand what they are asking for here
+
     let timer;
 
-    const counting = () => {
-      start++;
+    const iterate = () => { 
+      console.log(start++);
 
       if (start <= end) {
-        timer = setTimeout(counting, 100);
+        timer = setTimeout(iterate, 100);
       }
-    }
+    };
 
-    counting();
+    iterate();
 
     return {
-      cancel: () => clearTimeout(timer),
+      cancel: () => timer && clearTimeout(timer),
     }
   }
 };
